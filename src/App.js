903b552fb8e39519ea48d70a91042df242cad4fa
Jimmy Emery <jimmy.e@outlook.com>
2021-02-24 11:33:56 -0500

    updating css imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
+import { EuiPanel } from '@elastic/eui';
 
 const App = () => (
-  <h1>hello world</h1>
+  <EuiPanel>
+    <h1>hello world</h1>
+  </EuiPanel>
 );
 
 export default App;
